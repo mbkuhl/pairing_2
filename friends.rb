9049cf186a560c54d7ee264dee0fac4ba1b40071
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-07-12 19:29:15 -0500

    Add method

--- friends.rb
+++ friends.rb
@@ -2,3 +2,7 @@ arrays = ["a", "b", "c", "d", "e"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+array_places = ["texas", "florida", "DC", "ohio", "LA"]
+puts array_places.delete("LA")
+puts array_places
